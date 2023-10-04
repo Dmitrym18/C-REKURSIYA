a628968266b76c5c2c1c8b305839047fdb450da9
Dmitry Medvedev <medved-dmi@yandex.ru>
2023-10-04 12:20:57 +0300

    Amended homework

--- task 64/Program.cs
+++ task 64/Program.cs
@@ -13,29 +13,3 @@ Console.WriteLine(NaturalNumber(8, 1));
 
 
 
-
-// Задача 63: Задайте значение N.
-// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
-// N = 5 -> "1, 2, 3, 4, 5"
-// N = 6 -> "1, 2, 3, 4, 5, 6"
-
-
-// string ShowNumbers(int num)
-// {
-// if(num == 1) return "1 ";
-// else return ShowNumbers(num - 1) + $"{num} ";
-// }
-
-// void Show(int number)
-// {
-// if(number == 1) Console.Write(1 + " ");
-// else
-// {
-// Show(number - 1);
-// Console.Write(number + " ");
-
-// }
-// }
-
-// Console.WriteLine(ShowNumbers(5));
-// Show(6);
